8a18c6e0d1ad772f4a37a031d3b1ef13c394918a
qEmrys <takashiro.sv@gmail.com>
2026-02-16 23:06:58 +0200

    add get_numbers_ticket function

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 
 def get_days_from_today(target_date: str) -> int | None:
     if not isinstance(target_date, str):
@@ -13,3 +14,14 @@ def get_days_from_today(target_date: str) -> int | None:
     
     current_date = datetime.date.today()
     return (current_date - target_date).days
+
+def get_numbers_ticket(min, max, quantity):
+    if min < 1 or max > 1000:
+        return list()
+    
+    if not min < quantity < max:
+        return list()
+    
+    numbers = random.sample(range(min, max), quantity)
+    
+    return sorted(numbers)
