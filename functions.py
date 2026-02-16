cd0efeabe864ba5d4f03f4cb419c35b2a33d764f
qEmrys <takashiro.sv@gmail.com>
2026-02-16 22:37:53 +0200

    add exception handling for invalid date input

--- functions.py
+++ functions.py
@@ -1,7 +1,15 @@
 import datetime
 
-def get_days_from_today(target_date):
+def get_days_from_today(target_date: str) -> int | None:
+    if not isinstance(target_date, str):
+        return None
+    
     format_date = '%Y-%m-%d'
-    target_date = datetime.date.strptime(target_date, format_date)
+
+    try:
+        target_date = datetime.date.strptime(target_date, format_date)
+    except ValueError:
+        return None
+    
     current_date = datetime.date.today()
     return (current_date - target_date).days
