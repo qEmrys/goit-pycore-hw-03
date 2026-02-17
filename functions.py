a56d4085ce435c6d1f03753ec8b6eb7dcca3704e
qEmrys <takashiro.sv@gmail.com>
2026-02-17 21:33:26 +0200

    add get_upcoming_birthdays function

--- functions.py
+++ functions.py
@@ -36,3 +36,30 @@ def normalize_phone(phone_number: str) -> str:
         return '+' + parsed_number
     
     return '+38' + parsed_number
+
+def get_upcoming_birthdays(users: list) -> list:
+    format_date = '%Y.%m.%d'
+    congratulation_users = list()
+
+    for user in users:
+        current_date = datetime.date.today()
+        target_date = datetime.datetime.strptime(user['birthday'], format_date).date()
+        target_date_norm = target_date.replace(year=current_date.year)
+
+        if target_date_norm < current_date:
+            target_date_norm = target_date_norm.replace(year=current_date.year + 1)
+
+        days_to_birthday = (target_date_norm - current_date).days
+
+        if days_to_birthday <= 7:
+            congratulation_date = target_date_norm
+
+            if congratulation_date.weekday() >= 5:
+                days_to_add = 7 - congratulation_date.weekday()
+                congratulation_date = congratulation_date + datetime.timedelta(days=days_to_add)
+
+            congratulation_users.append(dict())
+            congratulation_users[-1]['name'] = user['name']
+            congratulation_users[-1]['congratulation_date'] = congratulation_date.strftime('%Y.%m.%d')
+        
+    return congratulation_users
